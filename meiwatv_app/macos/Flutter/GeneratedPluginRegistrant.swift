56f08cf178ddcc056b0bda0f050e382255e9545f
befoxgaming110-coder <befoxgaming110@gmail.com>
2026-09-17 16:21:40 +0800

    Update app icon assets, iOS icons, and blue branding theme #243F7C

--- meiwatv_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ meiwatv_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import cloud_firestore
 import firebase_core
 import package_info_plus
+import share_plus
 import sqflite_darwin
 import url_launcher_macos
 import video_player_avfoundation
@@ -18,6 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
