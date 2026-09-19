8e00b76ee241f3f348ecffb31a82f7f466b20806
befoxgaming110-coder <befoxgaming110@gmail.com>
2026-09-19 16:52:31 +0800

    Clean up all unused temporary test scripts, old debug dumps, and obsolete JSON files

--- dump-card.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-if (fs.existsSync('source_dump.html')) {
-  const html = fs.readFileSync('source_dump.html', 'utf8');
-  const cardRegex = /<div[^>]*class="[^"]*grid-matches__item[^"]*"[^>]*>([\s\S]*?)<\/div>\s*<\/div>\s*<\/div>/gi;
-  const m = cardRegex.exec(html);
-  if (m) {
-    console.log(m[0]);
-  }
-}
--- test-soco.js
+++
@@ -1,7 +0,0 @@
-async function parseSoco() {
-    const res = await fetch('https://socolivezc.tv/', { headers: { 'User-Agent': 'Mozilla/5.0' } });
-    const html = await res.text();
-    const matches = html.match(/\/truc-tiep\/[^\s"'<>]+/gi) || [];
-    console.log('Unique soco matches:', [...new Set(matches)]);
-}
-parseSoco();
